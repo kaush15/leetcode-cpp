b6ba7ca0da1b7cdd4e374c90318d62edf072d9c8
Kaushlendra Shukla <kaushlendrashukla1521@gmail.com>
2026-08-13 16:31:26 +0530

    Create product-of-array-except-self.cpp

---
+++ arrays/product-of-array-except-self.cpp
@@ -0,0 +1,22 @@
+// Problem: Product of Array Except Self
+// Link: https://leetcode.com/problems/product-of-array-except-self/description/
+// Approach: First calculate the prefix product then using the result and the original array calculate the product of array except self using the prefix and suffix variables
+// Time: O(n) | Space: O(1)
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int prefix=1;
+        vector<int> result(nums.size(),1);
+        
+        for(int i=0;i<nums.size();i++){
+            result[i]= prefix;
+            prefix= prefix * nums[i];
+        }
+        int suffix=1;
+        for(int i=nums.size()-1;i>=0;i--){
+            result[i]= result[i] * suffix;
+            suffix= suffix * nums[i];
+        }
+        return result;
+    }
+};
